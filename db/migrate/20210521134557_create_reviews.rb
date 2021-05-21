49c3d0d64dcb97c283dfeeb405078b663305af06
Christian Boehme <c.b.90@gmx.de>
2021-05-21 16:03:10 +0200

    ahhhhhhhh

--- db/migrate/20210521134557_create_reviews.rb
+++ db/migrate/20210521134557_create_reviews.rb
@@ -3,6 +3,7 @@ class CreateReviews < ActiveRecord::Migration[6.0]
     create_table :reviews do |t|
       t.string :comment
       t.integer :rating
+      t.references :list, null: false, foreign_key: true
 
       t.timestamps
     end
